7220ca1c20695ded67153792f103957dc4b8860a
Jan-willem De Bleser <jw@thescrapyard.org>
2022-02-22 16:29:36 -0800

    Clean up error handling

--- src/main.rs
+++ src/main.rs
@@ -47,9 +47,11 @@ type ControlSocket = Socket;
 type DataSocket = Socket;
 
 #[derive(Debug)]
-enum MyError {
+enum Error {
     SystemError(Errno),
-    NotABridge
+    PathIsNotABridge,
+    InvalidUtf8(Utf8Error),
+    NulError(String),
 }
 
 
@@ -63,11 +65,11 @@ struct NodeInfo {
 }
 
 impl TryFrom<nodeinfo> for NodeInfo {
-    type Error = Utf8Error;
+    type Error = Error;
 
     fn try_from(ninf: nodeinfo) -> Result<Self, Self::Error> {
-        let name  = unsafe{CStr::from_ptr(ninf.name.as_ptr())}.to_str()?.to_owned();
-        let type_ = unsafe{CStr::from_ptr(ninf.type_.as_ptr())}.to_str()?.to_owned();
+        let name  = unsafe{CStr::from_ptr(ninf.name.as_ptr())}.to_str().map_err(Error::InvalidUtf8)?.to_owned();
+        let type_ = unsafe{CStr::from_ptr(ninf.type_.as_ptr())}.to_str().map_err(Error::InvalidUtf8)?.to_owned();
         Ok(NodeInfo { name: name,
                       nodetype: type_,
                       id: ninf.id,
@@ -85,11 +87,11 @@ struct LinkInfo {
 }
 
 impl TryFrom<&linkinfo> for LinkInfo {
-    type Error = Utf8Error;
+    type Error = Error;
 
     fn try_from(linf: &linkinfo) -> Result<Self, Self::Error> {
-        let ourhook  = unsafe{CStr::from_ptr(linf.ourhook.as_ptr())}.to_str()?.to_owned();
-        let peerhook = unsafe{CStr::from_ptr(linf.peerhook.as_ptr())}.to_str()?.to_owned();
+        let ourhook  = unsafe{CStr::from_ptr(linf.ourhook.as_ptr())}.to_str().map_err(Error::InvalidUtf8)?.to_owned();
+        let peerhook = unsafe{CStr::from_ptr(linf.peerhook.as_ptr())}.to_str().map_err(Error::InvalidUtf8)?.to_owned();
         let peer = NodeInfo::try_from(linf.nodeinfo)?;
         Ok(LinkInfo { ourhook, peerhook, peer })
     }
@@ -132,26 +134,40 @@ unsafe fn ng_send_msg(csock: &ControlSocket, path: &CStr, cookie: c_int, cmd: c_
     Ok(())
 }
 
-unsafe fn command_response(csock: &ControlSocket, path: &str, cookie: c_int, cmd: c_int, parg: *const c_void, arglen: c_ulong) -> Result<*mut ng_mesg, Errno> {
-    let cpath = CString::new(path).unwrap();  // FIXME
-    ng_send_msg(csock, &cpath, cookie, cmd, parg, arglen)?;
-    let (msg, _) = ng_alloc_recv_msg(csock)?;
-    Ok(msg)
+unsafe fn ng_send_ascii_message(csock: &ControlSocket, path: &CStr, query: &CStr) -> Result<(), Errno> {
+    // FIXME multiple arguments in
+    let ret = NgSendAsciiMsg(csock.as_raw_fd(), path.as_ptr() as *const c_char, query.as_ptr() as *const c_char);
+    if ret == -1 {
+        return Err(errno())
+    }
+    Ok(())
 }
 
-fn list_hooks(csock: &ControlSocket, path: &str) -> Result<(NodeInfo, Vec<LinkInfo>), Errno> {
+fn command_response(csock: &ControlSocket, path: &str, cookie: c_int, cmd: c_int, parg: *const c_void, arglen: c_ulong) -> Result<*mut ng_mesg, Error> {
+    let cpath = CString::new(path).map_err(|_| Error::NulError(path.to_owned()))?;
+    Ok(unsafe {
+        ng_send_msg(csock, &cpath, cookie, cmd, parg, arglen).map_err(Error::SystemError)?;
+        let (msg, _) = ng_alloc_recv_msg(csock).map_err(Error::SystemError)?;
+        msg
+    })
+}
+
+fn list_hooks(csock: &ControlSocket, path: &str) -> Result<(NodeInfo, Vec<LinkInfo>), Error> {
     let parg: *const c_void = ptr::null();
-    let respptr: *mut ng_mesg = unsafe { command_response(csock, path, NGM_GENERIC_COOKIE as c_int, NGM_LISTHOOKS as c_int, parg, 0) }?;
+    let respptr: *mut ng_mesg = command_response(csock, path, NGM_GENERIC_COOKIE as c_int, NGM_LISTHOOKS as c_int, parg, 0)?;
 
     // Pointer should not be null at this point (check?)
     //let hdr = unsafe { (*respptr).header };
     Ok(unsafe {
-        let pdata: *const hooklist = (*respptr)
-                                        .data
-                                        .as_ptr()
-                                        .cast();
-        let ninf = NodeInfo::try_from((*pdata).nodeinfo).unwrap();
-        let mut links: Vec<LinkInfo> = (*pdata).link.as_slice(ninf.hooks.try_into().unwrap()).iter().map(|l| LinkInfo::try_from(l).unwrap()).collect();
+        let pdata: *const hooklist = (*respptr).data.as_ptr().cast();
+        let ninf = NodeInfo::try_from((*pdata).nodeinfo)?;
+        let tmp: Result<Vec<LinkInfo>, Error> = (*pdata)
+             .link
+             .as_slice(ninf.hooks.try_into().unwrap())
+             .iter()
+             .map(|l| LinkInfo::try_from(l))
+             .collect();
+        let mut links = tmp?;
         links.sort_unstable_by(|a, b| a.ourhook.cmp(&b.ourhook));
         // ng_mesg is a nested set of flexible-array structs, so one call to free() is enough
         free(respptr as *mut c_void);
@@ -159,10 +175,10 @@ fn list_hooks(csock: &ControlSocket, path: &str) -> Result<(NodeInfo, Vec<LinkIn
     })
 }
 
-fn find_free_hook(csock: &ControlSocket, path: &str) -> Result<usize, MyError> {
-    let (ninf, links) = list_hooks(csock, path).map_err(|e| MyError::SystemError(e))?;
+fn find_free_hook(csock: &ControlSocket, path: &str) -> Result<usize, Error> {
+    let (ninf, links) = list_hooks(csock, path)?;
     if ninf.nodetype != "bridge" {
-        return Err(MyError::NotABridge);
+        return Err(Error::PathIsNotABridge);
     }
     for (n, l) in links.iter().map(|l| l.ourhook[4..].parse::<u32>().unwrap()).enumerate() {
         if l > n.try_into().unwrap() {
@@ -172,66 +188,45 @@ fn find_free_hook(csock: &ControlSocket, path: &str) -> Result<usize, MyError> {
     Ok(ninf.hooks.try_into().unwrap())
 }
 
-fn make_peer(csock: &ControlSocket, path: &str, nodetype: &str, ourhook: &str, peerhook: &str) -> Result<(), Errno> {
-    let cs: c_int = csock.as_raw_fd();
-    let cpath = CString::new(path).unwrap();
+fn make_peer(csock: &ControlSocket, path: &str, nodetype: &str, ourhook: &str, peerhook: &str) -> Result<(), Error> {
+    let cpath = CString::new(path).map_err(|_| Error::NulError(path.to_owned()))?;
     let newpeer = ngm_mkpeer::from((nodetype, ourhook, peerhook));
     let pnewpeer: *const ngm_mkpeer = &newpeer;
 
     // send the makepeer command
-    let ret = unsafe { NgSendMsg(cs, cpath.as_ptr() as *const c_char, NGM_GENERIC_COOKIE as c_int, NGM_MKPEER as c_int, pnewpeer as *const c_void, size_of::<ngm_mkpeer>() as u64) };
-    if ret == -1 {
-        return Err(errno())
-    }
+    unsafe { ng_send_msg(csock, &cpath, NGM_GENERIC_COOKIE as c_int, NGM_MKPEER as c_int, pnewpeer as *const c_void, size_of::<ngm_mkpeer>() as u64).map_err(Error::SystemError)? };
+
     // no response expected
     Ok(())
 }
 
-fn get_interface_name(csock: &ControlSocket, path: &str, ourhook: &str) -> Result<CString, Errno> {
-    let cs: c_int = csock.as_raw_fd();
+fn get_interface_name(csock: &ControlSocket, path: &str, ourhook: &str) -> Result<CString, Error> {
     let mut linkpath = String::from(path); // already contains a colon at the end
     linkpath.push_str(&ourhook);
-    let clpath = CString::new(linkpath).unwrap();
-    let query = CString::new("getifname").unwrap();
-    let ret = unsafe { NgSendAsciiMsg(cs, clpath.as_ptr() as *const c_char, query.as_ptr() as *const c_char) };
-    if ret == -1 {
-        return Err(errno())
-    }
-
+    let clpath = CString::new(linkpath.clone()).map_err(|_| Error::NulError(linkpath))?;
+    let query = CString::new("getifname").unwrap();  // this should never fail
     unsafe {
-        let (binresp, rpath) = ng_alloc_recv_msg(csock)?;
+        ng_send_ascii_message(csock, &clpath, &query).map_err(Error::SystemError)?;
+        let (binresp, _) = ng_alloc_recv_msg(csock).map_err(Error::SystemError)?;
         let text: *const c_char = (*binresp).data.as_ptr().cast();
-        let device = CStr::from_ptr(text).to_owned();  // FIXME
-        //let ret = NgSendMsg(cs, rpath.as_ptr() as *const c_char, NGM_GENERIC_COOKIE as c_int, NGM_BINARY2ASCII as c_int, binresp as *const c_void, size_of::<ng_mesg>() as u64 + (*binresp).header.arglen as u64);
+        let device_name = CStr::from_ptr(text).to_owned();
         free(binresp as *mut c_void);
-        /*
-        if ret == -1 {
-            return Err(errno())
-        }
-        let (asciiresp, _) = ng_alloc_recv_msg(csock)?;
-        // this is weird: ng_mesg containing an ng_mesg? Taken from ngctl source, but...
-        let ascii: *const ng_mesg = (*asciiresp).data.as_ptr().cast();
-        let text: *const c_char = (*ascii).data.as_ptr().cast();
-        let device = CStr::from_ptr(text).to_owned();  // FIXME
-        free(asciiresp as *mut c_void);
-        */
-        Ok(device)
+        Ok(device_name)
     }
 }
 
-fn go(path: &str) -> Result<(), MyError> {
-    let (csock, _) = ng_mk_sock_node(format!("ngctlrs{}", id())).map_err(|e| MyError::SystemError(e))?;
-    unsafe { NgSetDebug(2) };
+fn go(path: &str) -> Result<String, Error> {
+    let (csock, _) = ng_mk_sock_node(format!("ngctlrs{}", id())).map_err(Error::SystemError)?;
+    //unsafe { NgSetDebug(2) };
     let link = format!("link{}", find_free_hook(&csock, path)?);
-    make_peer(&csock, path, "eiface", &link, "ether").map_err(|e| MyError::SystemError(e))?;
-    let device = get_interface_name(&csock, path, &link).map_err(|e| MyError::SystemError(e))?;
-    println!("{}", device.to_str().unwrap());
-    Ok(())
+    make_peer(&csock, path, "eiface", &link, "ether")?;
+    let device_name = get_interface_name(&csock, path, &link)?;
+    Ok(device_name.into_string().map_err(|e| Error::InvalidUtf8(e.utf8_error()))?)
 }
 
 fn main() {
     match go("bridge:") {
         Err(e) => println!("error: {:?}", e),
-        Ok(_) => ()
+        Ok(s) => println!("{}", s)
     }
 }
